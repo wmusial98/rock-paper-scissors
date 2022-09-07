6276896c92bb202d9abc57ec44c59bec24686beb
wmusial98 <wiktoria.musial@gmail.com>
2022-09-07 11:16:13 -0700

    Update game logic and create basic DOM structure.

--- game.js
+++ game.js
@@ -1,90 +1,74 @@
 let userInput = 0;
 let playerScore = 0;
 let compScore = 0;
+let compOptions = Array('rock', 'paper', 'scissors');
 let gameCount = 0;
+const buttons = document.querySelectorAll('button');
 
-game();
+const msgContainer = document.querySelector('#result');
+const resContent = document.createElement('p');
+resContent.classList.add('resContent');
+resContent.textContent = '';
+const gameCounter = document.createElement('p');
+gameCounter.classList.add('gameCounter');
+gameCounter.textContent = '';
 
-function game() {
-    while (compScore < 5 && playerScore < 5) {
-        let userChoice = playerSelection();
-        let compChoice = computerChoice();
-        if(userChoice == 'rock' || userChoice == 'paper' || userChoice == 'scissors') {
-            playRound(compChoice, userChoice);
-            console.log('You played ' + gameCount + ' games. The score is currently Computer - ' + compScore + ', Player - ' + playerScore);
+function playRound(e) {
+    let userPick = this.classList.value;
+    let compPick = computerChoice();
+    console.log(userPick, compPick);
+
+    if(userPick === compPick) {
+        resultMessage('tie');
+    } else if (userPick === 'rock') {
+        if(compPick === 'paper') {
+            resultMessage('loss');
         } else {
-            alert('Pick rock, paper, or scissors ONLY.');
+            resultMessage('win');
         }
+    } else if (userPick === 'paper') {
+        if(compPick === 'rock') {
+            resultMessage('win');
+        } else {
+            resultMessage('loss');
+        }
+    } else if (userPick === 'scissors') {
+        if(compPick === 'paper') {
+            resultMessage('win');
+        } else {
+            resultMessage('loss');
+        }
+    }
+}
+
+buttons.forEach(button => button.addEventListener('click', playRound));
+
+function computerChoice() {
+    return compOptions[Math.floor(Math.random()*compOptions.length)];
+}
+
+function resultMessage(resultMsg) {
+    if(resultMsg === 'win') {
+        console.log('win');
+    } else if (resultMsg === 'loss') {
+        console.log('loss');
+    } else if (resultMsg === 'tie') {
+        console.log('tie'); 
     } 
 }
 
-function playerSelection() {
-    userInput = prompt('Rock, Paper, or Scissors?');
-    let userChoice = userInput.toLowerCase();
-    return userChoice;
-}
+/* game();
 
-function computerChoice() {
-    /*https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript*/
-    /* generates a random number from 1 to 3 */
-    let compChoice = Math.floor(Math.random() * 3) + 1;
-    console.log(compChoice);
-    return compChoice;
+function game() {
+    let userChoice = playerSelection();
+    console.log(userChoice);
+    let compChoice = computerChoice();
+    console.log(userChoice + compChoice);
+    playRound(compChoice, userChoice);
+    console.log('You played ' + gameCount +
+        ' games. The score is currently Computer - ' + compScore +
+        ', Player - ' + playerScore);
 }
-
-function playRound(compPick, userPick) {
-    switch (compPick) {
-        case 1:
-            /*rock*/ 
-            switch(userPick) {
-                case 'rock':
-                    console.log('It is a tie!');
-                    break;
-                case 'paper':
-                    console.log('The computer picked rock. You win!');
-                    ++playerScore;
-                    break;
-                case 'scissors':
-                    console.log('The computer picked rock. You lose!');
-                    ++compScore;
-                    break;
-            }
-            gameCount++;
-            break;
-        case 2:
-            /*paper*/
-            switch(userPick) {
-                case 'rock':
-                    console.log('The computer picked paper. You lose!');
-                    ++compScore;
-                    break;
-                case 'paper':
-                    console.log('It is a tie!');
-                    break;
-                case 'scissors':
-                    console.log('The computer picked paper. You win!');
-                    ++playerScore;
-                    break;
-            }
-            gameCount++;
-            break;
-        case 3:
-            /*scissors*/
-            switch(userPick) {
-                case 'rock':
-                    console.log('The computer picked scissors. You win!');
-                    ++playerScore;
-                    break;
-                case 'paper':
-                    console.log('The computer picked scissors. You lose!');
-                    ++compScore;
-                    break;
-                case 'scissors':
-                    console.log('It is a tie!')
-                    break;
-            }
-            gameCount++;
-            break;
-    }
-    
-}
+function logText(e) {
+    console.log(this.classList.value);
+} */
