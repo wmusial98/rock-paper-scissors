dfe12b563fc32c0be477c51f93cb31bd080061d1
Wiktoria Musial <108089460+wmusial98@users.noreply.github.com>
2022-09-07 17:46:10 -0700

    Rps UI (#2)
    Add game UI

--- game.js
+++ game.js
@@ -1,90 +1,167 @@
 let userInput = 0;
 let playerScore = 0;
 let compScore = 0;
+let compOptions = Array('rock', 'paper', 'scissors');
 let gameCount = 0;
+const buttons = document.querySelectorAll('button');
 
-game();
+const playBoard = document.querySelector('.playboard');
+const compImg = document.createElement('img');
+const playerImg = document.createElement('img');
+compImg.classList.add('playImg');
+playerImg.classList.add('playImg');
 
-function game() {
-    while (compScore < 5 && playerScore < 5) {
-        let userChoice = playerSelection();
-        let compChoice = computerChoice();
-        if(userChoice == 'rock' || userChoice == 'paper' || userChoice == 'scissors') {
-            playRound(compChoice, userChoice);
-            console.log('You played ' + gameCount + ' games. The score is currently Computer - ' + compScore + ', Player - ' + playerScore);
+const msgContainer = document.querySelector('.result');
+const resContent = document.createElement('p');
+resContent.classList.add('resContent');
+resContent.textContent = '';
+const gameCounter = document.createElement('p');
+gameCounter.classList.add('gameCounter');
+gameCounter.textContent = '';
+
+const rightScore = document.querySelector('.rightScore');
+const leftScore = document.querySelector('.leftScore');
+const oppoScore = document.createElement('div');
+const yourScore = document.createElement('div');
+oppoScore.classList.add('oppoScore');
+yourScore.classList.add('yourScore');
+oppoScore.textContent = '0';
+yourScore.textContent = '0';
+rightScore.appendChild(yourScore);
+leftScore.appendChild(oppoScore);
+
+const endMess = document.querySelector('.result');
+
+const gameBegin = document.querySelector('.result');
+
+function playRound(e) {
+    if (compScore == 5 || playerScore == 5) {
+        if (compScore > playerScore) {
+            gameReset('The computer wins!');
         } else {
-            alert('Pick rock, paper, or scissors ONLY.');
+            gameReset('Congratulations! You win :)');
         }
-    } 
+    } else {
+        let userPick = this.classList.value;
+        let compPick = computerChoice();
+        updateImages(compPick, userPick);
+
+        if (userPick === compPick) {
+            resultMessage('tie');
+        } else if (userPick === 'rock') {
+            if (compPick === 'paper') {
+                resultMessage('loss');
+            } else {
+                resultMessage('win');
+            }
+        } else if (userPick === 'paper') {
+            if (compPick === 'rock') {
+                resultMessage('win');
+            } else {
+                resultMessage('loss');
+            }
+        } else if (userPick === 'scissors') {
+            if (compPick === 'paper') {
+                resultMessage('win');
+            } else {
+                resultMessage('loss');
+            }
+        }
+    }
 }
 
-function playerSelection() {
-    userInput = prompt('Rock, Paper, or Scissors?');
-    let userChoice = userInput.toLowerCase();
-    return userChoice;
-}
+buttons.forEach(button => button.addEventListener('click', playRound));
 
 function computerChoice() {
-    /*https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript*/
-    /* generates a random number from 1 to 3 */
-    let compChoice = Math.floor(Math.random() * 3) + 1;
-    console.log(compChoice);
-    return compChoice;
+    return compOptions[Math.floor(Math.random() * compOptions.length)];
 }
 
-function playRound(compPick, userPick) {
-    switch (compPick) {
-        case 1:
-            /*rock*/ 
-            switch(userPick) {
-                case 'rock':
-                    console.log('It is a tie!');
-                    break;
-                case 'paper':
-                    console.log('The computer picked rock. You win!');
-                    ++playerScore;
-                    break;
-                case 'scissors':
-                    console.log('The computer picked rock. You lose!');
-                    ++compScore;
-                    break;
-            }
-            gameCount++;
-            break;
-        case 2:
-            /*paper*/
-            switch(userPick) {
-                case 'rock':
-                    console.log('The computer picked paper. You lose!');
-                    ++compScore;
-                    break;
-                case 'paper':
-                    console.log('It is a tie!');
-                    break;
-                case 'scissors':
-                    console.log('The computer picked paper. You win!');
-                    ++playerScore;
-                    break;
-            }
-            gameCount++;
-            break;
-        case 3:
-            /*scissors*/
-            switch(userPick) {
-                case 'rock':
-                    console.log('The computer picked scissors. You win!');
-                    ++playerScore;
-                    break;
-                case 'paper':
-                    console.log('The computer picked scissors. You lose!');
-                    ++compScore;
-                    break;
-                case 'scissors':
-                    console.log('It is a tie!')
-                    break;
-            }
-            gameCount++;
-            break;
+function updateImages(compPick, userPick) {
+    compImg.src = './img/compimg/' + compPick + '.jpg';
+    playerImg.src = './img/' + userPick + '.jpg';
+    playBoard.appendChild(compImg);
+    playBoard.appendChild(playerImg);
+}
+
+function resultMessage(resultMsg) {
+    if (resultMsg === 'win') {
+        playerScore++;
+        resContent.textContent = 'You win!';
+        scoreUpdate();
+        textUpdate();
+        endCheck();
+    } else if (resultMsg === 'loss') {
+        compScore++;
+        resContent.textContent = 'You lose!';
+        scoreUpdate();
+        textUpdate();
+        endCheck();
+    } else if (resultMsg === 'tie') {
+        resContent.textContent = 'You tied!';
+        textUpdate();
+        endCheck();
     }
-    
+}
+
+function endCheck() {
+    if (compScore == 5 || playerScore == 5) {
+        if (compScore > playerScore) {
+            gameReset('The computer wins!');
+        } else {
+            gameReset('Congratulations! You win :)');
+        }
+    }
+}
+
+
+function textUpdate() {
+    msgContainer.appendChild(resContent);
+    gameCtUpdate();
+}
+
+function gameCtUpdate() {
+    ++gameCount;
+    gameCounter.textContent = 'You played ' + gameCount + ' games!';
+    msgContainer.appendChild(gameCounter);
+}
+
+function scoreUpdate() {
+    oppoScore.textContent = compScore;
+    yourScore.textContent = playerScore;
+    rightScore.appendChild(yourScore);
+    leftScore.appendChild(oppoScore);
+}
+
+function gameReset(endMessage) {
+    const finalMess = document.createElement('p');
+    finalMess.textContent = endMessage;
+    endMess.appendChild(finalMess);
+    buttons.forEach(button => button.removeEventListener('click', playRound, false));
+    gameStart();
+}
+
+function gameStart() {
+    gameCtUpdate();
+    const gameButton = document.createElement('button');
+    gameButton.classList.add('gameButton');
+    gameButton.textContent = 'Restart Game';
+    gameBegin.appendChild(gameButton);
+    gameButton.addEventListener('click', zeroSet);
+}
+
+function zeroSet(e) {
+    removeAllChildNodes(msgContainer);
+    removeAllChildNodes(endMess);
+    removeAllChildNodes(gameBegin);
+    playerScore = 0;
+    compScore = 0;
+    gameCount = 1;
+    scoreUpdate();
+    buttons.forEach(button => button.addEventListener('click', playRound));
+}
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
 }
